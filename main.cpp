91a4a9b56f58914679cb1d61486a848024d01226
califernication <fernando.cuellogarcia@yale.edu>
2022-06-14 11:37:57 -0400

    Initial implementation of the vec3 class for color geometry and point representation

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    const int image_width = 800;
+    const int image_height = 400;
 
     // Render
 
@@ -14,13 +14,13 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+            auto r = double(i) / (image_width);
+            auto g = double(j) / (image_height);
             auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
