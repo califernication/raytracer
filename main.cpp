c99bb7a3ca759d8c773143154fafd4fc717cad30
califernication <fernando.cuellogarcia@yale.edu>
2022-06-14 17:00:27 -0400

    Color test sphere based on its normal vectors

--- main.cpp
+++ main.cpp
@@ -4,20 +4,28 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const point& center, double radius, const ray& r) {
+double hit_sphere(const point& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
+    if (discriminant < 0) { // Check if negative root
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant) ) / a; // else retun the value
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point(0,0,-1), 0.5, r))
-        return color(0, 1, 0);
+    double t = hit_sphere(point(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 unit = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(unit.x()+1, unit.y()+1, unit.z()+1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
