7d7887001e9299f8468585033082bb246430109f
califernication <fernando.cuellogarcia@yale.edu>
2022-06-14 10:48:52 -0400

    Implemented a progress indicator for the output

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,4 +25,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std::cerr << "\nDone.\n";
 }
